1ea48caec2628f0bd4c36bee037329a59599dcd8
desatinar <rodrigosdn@gmail.com>
2022-03-18 11:12:02 -0300

    ajeitado os erros vindos do mysql, o Authenticator e adicionado o ts-node-dev para o hot reload

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -21,8 +21,8 @@ export class UserController {
             res.status(200).send({ token });
 
         } catch (error) {
-            if (error instanceof BaseError)
-            res.status(400).send({ error: error.message });
+            const err = error as BaseError
+            res.status(400).send({ error: err.message });
         }
 
         await BaseDatabase.destroyConnection();
@@ -43,8 +43,8 @@ export class UserController {
             res.status(200).send({ token });
 
         } catch (error) {
-            if (error instanceof BaseError)
-            res.status(400).send({ error: error.message });
+            const err = error as BaseError
+            res.status(400).send({ error: err.message });
         }
 
         await BaseDatabase.destroyConnection();
