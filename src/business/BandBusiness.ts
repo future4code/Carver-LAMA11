6d5aff030f119cde5c943e98b9ec72eec9b03968
Karen Gaspar <karenasgaspar@gmail.com>
2022-03-19 21:43:13 -0300

    Testes

--- src/business/BandBusiness.ts
+++ src/business/BandBusiness.ts
@@ -38,7 +38,11 @@ export class BandBusiness {
         return tokenData;
     }
 
-    async getBandById(id: string) {
+    async getBandById(id: string, token: string) {
+
+        // if (!token) {
+        //     throw new Error("Token inválido ou não passado nos headers")
+        // }
 
         if (!id) {
             throw new Error("Id inválido ou não passado nos params")
@@ -46,6 +50,9 @@ export class BandBusiness {
 
         const bandDatabase = new BandDatabase();
         const result = await bandDatabase.getBandById(id);
+
+        // const authenticator = new Authenticator();
+        // const tokenData = authenticator.getData(token);
 
         if (!result) {
             throw new Error("Id inválido ou Post não encontrado")
